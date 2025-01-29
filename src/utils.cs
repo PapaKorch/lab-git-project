285730956d489e2dee90392c55c0a94c327a3aec
PapaKorch <71691793+PapaKorch@users.noreply.github.com>
2025-01-29 20:38:42 +0300

    Add feature add numbers

---
+++ src/utils.cs
@@ -0,0 +1,11 @@
+Console.WriteLine("Ну и что ты тут забыл?");
+
+public int add_numbers (int a, int b){
+    int i = a + b;
+    return(i);
+}
+
+public float multiple_numbers (int a, int b){
+    float i = a * b;
+    return(i);
+}
