988860c5ed4ad90109f09025de3c9d210ca4a94f
PapaKorch <71691793+PapaKorch@users.noreply.github.com>
2025-01-29 20:39:55 +0300

    Add feature multiple numbers

---
+++ src/main.cs
@@ -0,0 +1,6 @@
+Console.WriteLine("Введите числа");
+int a = Console.ReadLine();
+int b = Console.ReadLine();
+
+Console.WriteLine("Сложение - ", add_numbers(a,b));
+Console.WriteLine("Умножение - ", multiple_numbers(a,b));
